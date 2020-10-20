3f0d55a410870317a64c55b8062cb791f79d9cce
afiqsazlan <afiqsazlan@gmail.com>
2020-10-20 13:13:49 +0800

    Updated class list on Teacher > Class page

--- src/views/teachers/TeacherHome/TeacherHome.vue
+++ src/views/teachers/TeacherHome/TeacherHome.vue
@@ -1,7 +1,7 @@
 <template>
 
-  <teacher-dashboard>
-    <div class="px-5">
+  <teacher-dashboard class="pt-10">
+    <div class="px-5 ">
       <div class="w-3/4">
         <icon-base width="190" height="70" icon-name="app-logo" view-box="0 0 320 18">
           <AppLogo/>
