a74f9f5c924e1ce2cc8c8366e5615028b611f515
afiqsazlan <afiqsazlan@gmail.com>
2020-10-27 11:14:48 +0800

    Adjusted bottom navbar height

--- src/components/BottomNavbar/BottomNavbar.vue
+++ src/components/BottomNavbar/BottomNavbar.vue
@@ -1,32 +1,32 @@
 <template>
   <section id="bottom-navigation"
-           class="md:hidden block fixed inset-x-0 border-t-1/4 bottom-0 z-10 shadow bg-white h-20">
+           class="md:hidden block fixed inset-x-0 border-t-1/4 bottom-0 z-10 shadow bg-white h-14">
 
     <div id="tabs" class="flex h-full">
 
       <router-link :to="{name: 'teacher.home'}" class="w-1/5 pt-1 relative pl-8" exact>
-        <bottom-navbar-icon-base tab-name="home" class="absolute h-full w-3/4" icon-name="app-logo" view-box="0 5 60 55">
+        <bottom-navbar-icon-base tab-name="home" class="absolute h-full w-3/4" icon-name="app-logo" view-box="0 0 60 55">
           <HomeIcon/>
         </bottom-navbar-icon-base>
       </router-link>
       <router-link :to="{name: 'teacher.classes'}" class="w-1/5 pt-1 relative pl-5" exact>
-        <bottom-navbar-icon-base tab-name="classes" class="absolute h-full w-3/4" icon-name="app-logo" view-box="-5 5 60 55"
+        <bottom-navbar-icon-base tab-name="classes" class="absolute h-full w-3/4" icon-name="app-logo" view-box="-5 0 60 55"
                                  stroke-color="#2DEDEA">
           <ClassIcon/>
         </bottom-navbar-icon-base>
       </router-link>
-      <router-link :to="{name: 'teacher.students'}" class="w-1/5 pt-1 relative pl-3" exact>
-        <bottom-navbar-icon-base tab-name="students" class="absolute h-full w-3/4" icon-name="app-logo" view-box="0 5 60 55">
+      <router-link :to="{name: 'teacher.students'}" class="w-1/5  pt-1 relative pl-3" exact>
+        <bottom-navbar-icon-base tab-name="students" class="absolute h-full w-3/4" icon-name="app-logo" view-box="0 0 60 55">
           <StudentIcon/>
         </bottom-navbar-icon-base>
       </router-link>
-      <router-link :to="{name: 'teacher.assignments'}" class="w-1/5 pt-1 relative pl-3" exact>
-        <bottom-navbar-icon-base tab-name="assignments" class="absolute h-full w-3/4" icon-name="app-logo" view-box="0 5 60 55">
+      <router-link :to="{name: 'teacher.assignments'}" class="w-1/5  pt-1 relative pl-3" exact>
+        <bottom-navbar-icon-base tab-name="assignments" class="absolute h-full w-3/4" icon-name="app-logo" view-box="0 0 60 55">
           <AssignmentIcon/>
         </bottom-navbar-icon-base>
       </router-link>
-      <router-link :to="{name: 'teacher.settings'}" class="w-1/5 pt-1 relative pl-5" exact>
-        <bottom-navbar-icon-base tab-name="settings" class="absolute h-full w-3/4" icon-name="app-logo" view-box="0 5 60 55">
+      <router-link :to="{name: 'teacher.settings'}" class="w-1/5  pt-1 relative pl-5" exact>
+        <bottom-navbar-icon-base tab-name="settings" class="absolute h-full w-3/4" icon-name="app-logo" view-box="0 0 60 55">
           <SettingIcon/>
         </bottom-navbar-icon-base>
       </router-link>
