48658cd8678449e193dd4c00c9f0c08178ebb403
afiqsazlan <afiqsazlan@gmail.com>
2020-12-14 14:37:04 +0800

    Added marking feedback for student show marking

--- src/views/students/StudentAssignments/Marked/Index.vue
+++ src/views/students/StudentAssignments/Marked/Index.vue
@@ -1,31 +1,54 @@
 <template>
-  <div class="h-screen">
 
-    <!-- HEADER -->
-    <page-header-three :bottom-padding="8"
-    >
+  <dashboard-layout :has-fixed-header="true" :has-custom-bottom-bar="true">
 
-      <template v-slot:leftAction>
-        <nav-back :to="{name: 'student.assignments.show', params: {assignmentID: assignmentID}}"
-                  class="w-1/4"
-        />
-      </template>
-    </page-header-three>
 
-    <!-- CONTENT -->
-    <div class="relative pb-16/9 top-24">
+    <template v-slot:pageHeader>
+      <page-header-three class="pb-4">
+        <template v-slot:leftAction>
+          <nav-back class="w-2/7" stroke-color="red-primary"/>
+        </template>
 
-      <!--  ASSIGNMENT ANSWERS-->
+        <template v-slot:mini-title>
+          {{ pageTitle }}
+        </template>
 
-      <div class="pt-4 z-10 bg-white px-6 pb-16">
-        <answer-preview-swiper
-          :images="details.markingPicturePaths"
-        />
+      </page-header-three>
+    </template>
+
+    <!--  ASSIGNMENT ANSWERS-->
+    <template v-slot:content>
+
+      <!-- SHOW MARKINGS -->
+      <div v-if="isShowingMarkings"
+           class="relative pb-16/9 top-24">
+        <div class="pt-4 z-10 bg-white px-6 pb-16">
+          <answer-preview-swiper
+              :images="details.markingPicturePaths"
+          />
+        </div>
       </div>
-    </div>
 
+      <!-- SHOW FEEDBACK -->
+      <div v-if="isShowingFeedback"
+           class="relative px-8"
+           :class="details.feedback ? 'top-26 text-purple-primary text-left' : 'top-30 text-purple-secondary text-center' "
+      >
+        {{ details.feedback || 'No feedback available.' }}
+      </div>
+    </template>
 
-  </div>
+    <template v-slot:bottomBar>
+
+      <div class="w-full flex flex-row">
+        <button @click="toggleMode"
+                class="w-full font-bold rounded-full text-purple-primary text-sm border-2 border-purple-primary bg-white py-3 px-1 flex flex-row justify-center">
+          {{ buttonText }}
+        </button>
+      </div>
+
+    </template>
+  </dashboard-layout>
 </template>
 
 <script>
@@ -44,8 +67,34 @@ export default {
   },
   data() {
     return {
+
+      // states
+      isShowingMarkings: true,
+      isShowingFeedback: false,
+
       details: {
-        markingPicturePaths: []
+        markingPicturePaths: [],
+        feedback: null
+      }
+    }
+  },
+  computed: {
+    pageTitle() {
+      if (this.isShowingMarkings) {
+        return 'Markings'
+      } else if (this.isShowingFeedback) {
+        return 'Feedback'
+      } else {
+        return ''
+      }
+    },
+    buttonText() {
+      if (this.isShowingMarkings) {
+        return 'View Feedback'
+      } else if (this.isShowingFeedback) {
+        return 'View Markings'
+      } else {
+        return ''
       }
     }
   },
@@ -56,8 +105,13 @@ export default {
             let data = response.data;
 
             this.details.markingPicturePaths = data.marks_details.marking_picture_url.split(',');
+            this.details.feedback = data.marks_details.marks_feedback;
 
           })
+    },
+    toggleMode() {
+      this.isShowingFeedback = !this.isShowingFeedback;
+      this.isShowingMarkings = !this.isShowingMarkings;
     }
   },
   mounted() {
