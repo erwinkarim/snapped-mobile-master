eba11165c8a026e03d9d4b8c8ad134bf0298107b
afiqsazlan <afiqsazlan@gmail.com>
2021-03-08 15:54:15 +0800

    Handling google integration success

--- src/views/integrations/googleAuthCallback.vue
+++ src/views/integrations/googleAuthCallback.vue
@@ -1,19 +1,75 @@
 <template>
-  <div>
-    test
+  <div class="mx-auto h-screen flex flex-col justify-center items-center">
+    <div class="font-bold my-3 text-xl">
+      {{ title }}
+    </div>
+    <div class="text-sm">
+      {{ message }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "googleAuthCallback",
+  data() {
+    return {
+
+      // State
+      isAuthenticating: true,
+      isAuthenticated: false,
+
+      title: 'Authenticating...',
+      message: "Please wait. Thank you."
+    }
+  },
   methods: {
     googleClassroomCallback() {
       let code = this.$route.query.code;
+
       this.$store.dispatch('googleClassroom/integrationCallback', code)
+          .then(response => {
+
+            this.isAuthenticating = false;
+
+            console.log('received from backend callback')
+            console.log(response.data)
+            if (response.data.success) {
+
+              this.title = 'Success!'
+              this.message = response.data.message
+
+              console.log(`Callback page:${localStorage.getItem('token')}`)
+
+              setTimeout(() => {
+                this.$store.dispatch('googleClassroom/handleIntegrationSuccess')
+                // window.close();
+              }, 1500)
+
+            } else {
+
+              this.title = 'Oops!'
+              this.message = response.data.message
+
+              setTimeout(() => {
+                window.close();
+              }, 1500)
+            }
+          })
+          .catch( (error) => {
+
+            this.isAuthenticating = false;
+
+            this.title = 'Oops!'
+            this.message = error;
+          });
+
+
     }
   },
   created() {
+    console.log('created callback page')
+    console.log(this.$store.getters.getAuthUser)
     this.googleClassroomCallback();
   }
 }
