cf97ce431f92510e31cabf9bf43571f0eff8684d
Erwin Karim <erwinkarim@gmail.com>
2023-08-09 10:48:08 +0800

    add example charts for viz

--- src/components/Analytics/PercentileHistogram.vue
+++ src/components/Analytics/PercentileHistogram.vue
@@ -1,12 +1,55 @@
 <template>
 	<div>
+		<canvas :id="canvasName" width="400" height="400"></canvas>
 		<p>Show histogram (x-axis: score, y-axis, how many people got this score)</p>
 		<p>based on mode, individual: show position in histogram, group: just show distribution</p>
 	</div>
 </template>
 
 <script>
+import Chart from 'chart.js';
+
 export default {
 	name: 'PrecentileHistogram',
+	mounted(){
+		let ctx = document.getElementById(this.canvasName);
+		let data = {
+			// labels: [1,2,3,4,5,6,7],
+			datasets: [{
+				label: 'score vs count',
+				data: [
+					{x: 1, y: 1}, 
+					{x: 2, y: 2}, 
+					{x: 3, y: 3}, 
+					{x: 4, y: 4}, 
+					{x: 5, y: 3}, 
+					{x: 6, y: 2}, 
+					{x: 7, y: 1}, 
+				]
+			}]
+		};
+
+
+		let options = {
+			scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
+            }]
+        }
+		};
+
+		var myLineChart = new Chart(ctx, {
+			type: 'line',
+			data: data,
+			options: options
+		});
+
+	},
+	data() {
+		return {
+			canvasName: "myCanvas" + parseInt(Math.random() * 100 ), 
+		}
+	}
 }
 </script>
--- src/components/Analytics/Question.vue
+++ src/components/Analytics/Question.vue
@@ -1,5 +1,6 @@
 <template>
 	<div class="text-left bg-gray-secondary max-w-sm md:max-w-xl rounded rounded-xl justify-between overflow-hidden flex flex-col px-3 py-3 md:py-5 ">
+		<canvas :id="canvasName" width="400" height="400"></canvas>
 		<p>
 			Question X
 		</p>
@@ -9,7 +10,51 @@
 </template>
 
 <script>
+import Chart from 'chart.js';
+
 export default {
-	name: 'QuestionAnalytics'
+	name: 'QuestionAnalytics',
+	mounted() {
+		// draw the chart from data
+		var ctx = document.getElementById(this.canvasName);
+		console.log("myChart", this.canvasName);
+		var myChart = new Chart(ctx, {
+			type: 'horizontalBar',
+			data: {
+					labels: ['A', 'B', 'C', 'D'],
+					datasets: [{
+							label: '# of Votes',
+							data: [12, 19, 3, 5, 2, 3],
+							backgroundColor: [
+									'rgba(255, 99, 132, 0.2)',
+									'rgba(75, 192, 192, 0.2)',
+									'rgba(255, 99, 132, 0.2)',
+									'rgba(255, 99, 132, 0.2)',
+							],
+							borderColor: [
+									'rgba(255, 99, 132, 1)',
+									'rgba(54, 162, 235, 1)',
+									'rgba(255, 99, 132, 1)',
+									'rgba(255, 99, 132, 1)',
+							],
+							borderWidth: 1
+					}]
+			},
+			options: {
+					scales: {
+							yAxes: [{
+									ticks: {
+											beginAtZero: true
+									}
+							}]
+					}
+			}
+		});
+	},
+	data() {
+		return {
+			canvasName: "myCanvas" + parseInt(Math.random() * 100 ), 
+		}
+	}
 }
 </script>
